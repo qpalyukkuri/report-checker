d1cb93095e6e1b52213dcb7b7d5a59964173a8ea
Kenta Murayama <muraken063@sp.nitech.ac.jp>
2017-01-05 13:50:13 +0900

    test

--- proverbs.c
+++ proverbs.c
@@ -12,5 +12,6 @@ int main(int argc, char *argv[]) {
   printf("If you can dream it, you can do it.\n");
   printf("When in Rome, do as the Romans do.\n");
   printf("A man is not finished when he's defeated, he's finished when he quits.\n");
+  printf("The greatest lesson in life is to know that even fools are right sometimes.\n");
   return 0;
 }
