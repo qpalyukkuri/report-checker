1706c33b8132ba0f1f3c38be569a737b6952e0cd
hosaka-NIT <sikyouhinbousi@yahoo.co.jp>
2017-01-05 16:30:54 +0900

    add

--- proverbs.c
+++ proverbs.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
   printf("To be punctual is one of our social duties.\n");
   printf("Stay hungry, stay foolish.\n");
   printf("There is more to life than increasing its speed.\n");
+  printf("Be quick, but don't hurry. \n");
 
   return 0;
 }
