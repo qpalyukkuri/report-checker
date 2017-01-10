f9f09793ff8661d8c435c60bda910a61f36f7f2c
kurisyou625 <awata@mmsp.nitech.ac.jp>
2017-01-10 16:21:24 +0900

    commit
    add proverb

--- proverbs.c
+++ proverbs.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[]) {
   printf("In the middle of difficulty lies opportunity.\n");
   printf("Keep looking. Don't settle.\n");
   printf("Peace begins with a smile.\n");
+  printf("Living is not breathing but doing.\n");
 
   return 0;
 }
