6521b2daa7bb55d8a2a6198039c6d4c18d9a6df8
qpalyukkuri <qpal.yukkuri@gmail.com>
2017-01-13 04:13:27 +0900

    Add files via upload

---
+++ proverbs.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  printf("To be punctual is one of our social duties.\n");
+  printf("Stay hungry, stay foolish.\n");
+  printf("There is more to life than increasing its speed.\n");
+  printf("Rome was not built in a day.\n");
+  printf("If you can dream it, you can do it.\n");
+  printf("Ask, and it will be given to you.\n");
+  printf("A goal without a plan is just a wish..\n");
+  printf("If you can dream it, you can do it.\n");
+  printf("When in Rome, do as the Romans do.\n");
+  printf("A man is not finished when he's defeated, he's finished when he quits.\n");
+  printf("Success has ruined many a man.\n");
+  printf("Maintain the current situation is just go backwards. by Walt Disney\n");
+  printf("The greatest lesson in life is to know that even fools are right sometimes.\n");
+  printf("You'll never find a rainbow if you're looking down.\n");
+  printf("Be quick, but don't hurry. \n");
+  printf("In the middle of difficulty lies opportunity.\n");
+  printf("Keep looking. Don't settle.\n");
+  printf("Peace begins with a smile.\n");
+  printf("A man and a woman marry because both of them don’t know what to do with themselves.\n");
+  printf("Time is money.\n");  
+  printf("Time is money.\n");
+  printf("The book of nature is written in the language of mathematics.\n");
+  printf("Move fast and break things.\n");
+  printf("A woman does not become interesting until she is over 40.\n");
+  printf("He who moves not forward, goes backward.\n");
+  printf("Living is not breathing but doing.\n");
+  printf("Time is money.\n");
+  printf("The early bird catches the worm.\n");
+  printf("One good turn deserves another.\n");
+  printf("laugh and grow fat.\n")
+  printf("Boys be ambitious.\n");
+  printf("If you can dream it, you can do it.\n");
+  printf("Indecision is often worse than wrong action.\n");
+  printf("Love the life you live. Live the life you love.\n");
+  printf("Evrything is practice\n");
+  printf("Everything is practice.\n");
+  printf("There is always light behind the clouds\n.");
+  printf("When you come to a roadblock, take a detour.\n.");
+
+  return 0;
+}
