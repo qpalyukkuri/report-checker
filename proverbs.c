4e9dd82832a5602241a83792ad45fef3fc55e793
funaryo0907 <funaryo0907@gmail.com>
2017-01-12 20:58:01 +0900

    add proverb

--- proverbs.c
+++ proverbs.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
   printf("Time is money.\n");
   printf("The early bird catches the worm.\n");
   printf("One good turn deserves another.\n");
-  printf("laugh and grow fat.\n")
+  printf("laugh and grow fat.\n");
   printf("Boys be ambitious.\n");
   printf("If you can dream it, you can do it.\n");
   printf("Indecision is often worse than wrong action.\n");
